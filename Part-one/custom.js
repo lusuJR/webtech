3b3b9fbd3402d2f98bc65bcf10cb03f5effc303e
Selemani <lusukama.s@gmail.com>
2025-01-16 15:04:19 +0200

    add welcome page and add welcome page css

--- Part-one/custom.js
+++ Part-one/custom.js
@@ -1,27 +1,42 @@
+function validate() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-        function validate() {
-            var username = document.getElementById('username').value;
-            var password = document.getElementById('password').value;
+    // Reset error messages
+    document.getElementById("username_error").innerHTML = "";
+    document.getElementById("password_error").innerHTML = "";
 
-            // Reset error messages
-            document.getElementById("username_error").innerHTML = "";
-            document.getElementById("password_error").innerHTML = "";
+    // Validate username and password
+    if (username == '') {
+        document.getElementById("username_error").style.color = 'red';
+        document.getElementById("username_error").innerHTML = "* Username is required";
+    }
 
-            // Validate username
-            if (username == '') {
-                document.getElementById("username_error").style.color = 'red';
-                document.getElementById("username_error").innerHTML = "* Username is required";
-            }
+    if (password == '') {
+        document.getElementById("password_error").style.color = 'red';
+        document.getElementById("password_error").innerHTML = "* Password is required";
+    }
 
-            // Validate password
-            if (password == '') {
-                document.getElementById("password_error").style.color = 'red';
-                document.getElementById("password_error").innerHTML = "* Password is required";
-            }
-
-            // If both fields are filled, display welcome message
-            if (username != '' && password != '') {
-                document.getElementById("welcome_message").innerHTML = "Welcome " + username;
-            }
-        }
+    // If both fields are filled, redirect to the welcome page
+    if (username != '' && password != '') {
+        // Redirect to the welcome page, passing the username as a query parameter
+        window.location.href = 'welcome.html?username=' + encodeURIComponent(username);
+    }
+}
     
+
+// Function to retrieve query parameter from URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Display welcome message with the username
+window.onload = function() {
+    var username = getQueryParam('username');
+    if (username) {
+        document.getElementById("welcome_message").innerHTML = "Welcome, " + username + "!";
+    } else {
+        document.getElementById("welcome_message").innerHTML = "Welcome, Guest!";
+    }
+};
